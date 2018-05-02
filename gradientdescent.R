7c2ac89f1566a7ad3d81616e58f79595c8cf5b97
leejunta <leejunta@grinnell.edu>
2018-05-02 15:12:12 -0500

    starting gradietn descent

---
+++ gradientdescent.R
@@ -0,0 +1,9 @@
+#> sessionInfo()
+#R version 3.3.2 (2016-10-31)
+#Platform: x86_64-apple-darwin13.4.0 (64-bit)
+#Running under: macOS  10.13.4
+#
+#locale:
+#    [1] en_US.UTF-8/en_US.UTF-8/en_US.UTF-8/C/en_US.UTF-8/en_US.UTF-8
+
+if (dir.exists(PATH <- "~/Documents/Grinnell College/2017-2018/Spring/MAT336/"))
