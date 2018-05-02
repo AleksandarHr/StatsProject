6c27e64ceae0cce26c5cf9dbd8e36085e958c7dc
leejunta <leejunta@grinnell.edu>
2018-05-02 15:37:18 -0500

    adding files

--- gradientdescent.R
+++ gradientdescent.R
@@ -6,4 +6,24 @@
 #locale:
 #    [1] en_US.UTF-8/en_US.UTF-8/en_US.UTF-8/C/en_US.UTF-8/en_US.UTF-8
 
-if (dir.exists(PATH <- "~/Documents/Grinnell College/2017-2018/Spring/MAT336/"))
+### Functions for Gradient Descent
+###    costSVM (X, W, y, C, regularization)
+###    
+
+# set proper working directory
+if (dir.exists (PATH <- "~/Documents/Grinnell College/2017-2018/Spring/MAT336/finalproject/")) {
+    setwd (PATH)
+    source ("helpers.R")
+    library (foreach)
+} else {
+    stop ("Directory not found")
+}
+
+# cost function J for gradient descent using hinge loss
+#    and regularization function
+costSVM <- function (X, W, y, C, 
+                     regularization = (function (weights) 0)) {
+    return (regularization (W) + C * sum (max (0, 1 - y * (X %*% W))))
+}
+
+# gradient function of J
